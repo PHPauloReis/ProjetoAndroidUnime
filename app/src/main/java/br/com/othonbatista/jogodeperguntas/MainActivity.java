2c3b95f628a6eae119c97b771541c4b7d26cfe30
Othon Batista <othonbatista@gmail.com>
2022-05-20 22:36:50 -0300

    Botões de mudança de fragments implementados

--- app/src/main/java/br/com/othonbatista/jogodeperguntas/MainActivity.java
+++ app/src/main/java/br/com/othonbatista/jogodeperguntas/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.othonbatista.jogodeperguntas;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, new JogarFragment())
+                .commit();
+    }
+
+    public void buttonCadastrarJogarFragment(View v) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, new CadastrarFragment())
+                .commit();
+    }
+
+    public void buttonJogarCadastrarFragment(View v) {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.frameLayout, new JogarFragment())
