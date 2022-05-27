b2552e5fb941f88090eddd9881d41ff66a815148
Othon Batista <othonbatista@gmail.com>
2022-05-27 20:32:04 -0300

    Erro da dependência room com o androidx resolvido

--- app/src/main/java/br/com/othonbatista/jogodeperguntas/Questoes.java
+++ app/src/main/java/br/com/othonbatista/jogodeperguntas/Questoes.java
@@ -1,11 +1,9 @@
 package br.com.othonbatista.jogodeperguntas;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
 
-import lombok.Data;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-@Data
 @Entity
 public class Questoes {
 
@@ -15,4 +13,28 @@ public class Questoes {
 
     private String pergunta;
     private String resposta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
 }
